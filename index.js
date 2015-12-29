d7124f9524012bf334eb3632b4dbfb0db4fd4acf
Michelle Todd <michelle@khanacademy.org>
2015-12-28 20:49:38 -0800

    Use local version of React Native Web while waiting for style pull request to merge
    auditors: alpert

--- index.js
+++ index.js
@@ -4,25 +4,10 @@
 'use strict';
 
 import React, { StyleSheet } from 'react-native';
-import ReactDOM from 'react-dom';
 import Promise from 'promise';
 
 global.Promise = Promise;
 
 import MathFactsApp from './components/MathFactsApp';
 
-if (document) {
-  const head = document.head || document.getElementsByTagName('head')[0];
-  const style = document.createElement('style');
-  const css = StyleSheet.renderToString();
-  style.type = 'text/css';
-  if (style.styleSheet){
-    style.styleSheet.cssText = css;
-  } else {
-    style.appendChild(document.createTextNode(css));
-  }
-
-  head.appendChild(style);
-}
-
-ReactDOM.render(<MathFactsApp />, document.getElementById('content'));
+React.render(<MathFactsApp />, document.getElementById('content'));
